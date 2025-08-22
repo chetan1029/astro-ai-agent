efc23b3c8ed022539c564e3879e88b0aa68176e3
Chetan <chetanbadgujar92@gmail.com>
2025-08-22 12:56:10 +0200

    added whatsapp endpoint

--- app/src/whatsapp/api.py
+++ app/src/whatsapp/api.py
@@ -11,11 +11,11 @@ router = APIRouter()
 
 
 @router.get("/webhook")
-async def verify_webhook(
-    hub_mode: str = None,
-    hub_challenge: str = None,
-    hub_verify_token: str = None
-):
+async def verify_webhook(request: Request):
+    hub_mode = request.query_params.get("hub.mode")
+    hub_verify_token = request.query_params.get("hub.verify_token")
+    hub_challenge = request.query_params.get("hub.challenge")
+
     if hub_mode == "subscribe" and hub_verify_token == get_settings().whatsapp_verify_token:
         return PlainTextResponse(content=hub_challenge, status_code=200)
     return PlainTextResponse(content="Verification failed", status_code=403)
